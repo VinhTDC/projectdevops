3c9d105f737ce4491d4cff8bdc5cbac6283e6018
ThanhTa2ngan4 <tahynhatthanh2004@gmail.com>
2024-05-18 14:49:20 +0700

    add full

--- my-app/src/components/Products.jsx
+++ my-app/src/components/Products.jsx
@@ -1,6 +1,17 @@
 export default function Products() {
     const products = [
-        { id: 1, name: "Beauty Brush", description: "incididunt ut labore et dolore magna aliqua. Ut enim", image: "images/img-1.png", price: 30 },
+        { id: 1, name: "Cọ Trang Điểm", description: "Dùng để trang điểm mặt, dễ sử dụng.", image: "images/img-1.png", price: 30 },
+        { id: 2, name: "Son Môi", description: "Màu đỏ rực rỡ, không gây khô môi.", image: "images/img-2.png", price: 20 },
+        { id: 3, name: "Phấn Má", description: "Tạo vẻ hồng hào tự nhiên cho gương mặt.", image: "images/img-3.png", price: 25 },
+        { id: 4, name: "Mascara", description: "Làm dài và dày mi một cách tự nhiên.", image: "images/img-4.png", price: 15 },
+        { id: 5, name: "Mascara", description: "Làm dài và dày mi một cách tự nhiên.", image: "images/img-5.png", price: 15 },
+        { id: 6, name: "Mascara", description: "Làm dài và dày mi một cách tự nhiên.", image: "images/img-6.png", price: 15 },
+        { id: 7, name: "Mascara", description: "Làm dài và dày mi một cách tự nhiên.", image: "images/img-7.png", price: 15 },
+        { id: 8, name: "Mascara", description: "Làm dài và dày mi một cách tự nhiên.", image: "images/img-8.png", price: 15 },
+        { id: 9, name: "Mascara", description: "Làm dài và dày mi một cách tự nhiên.", image: "images/img-9.png", price: 15 },
+        { id: 10, name: "Mascara", description: "Làm dài và dày mi một cách tự nhiên.", image: "images/img-10.png", price: 15 },
+        { id: 11, name: "Mascara", description: "Làm dài và dày mi một cách tự nhiên.", image: "images/img-11.png", price: 15 },
+        { id: 12, name: "Mascara", description: "Làm dài và dày mi một cách tự nhiên.", image: "images/img-12.png", price: 15 },
     ];
 
     return (
